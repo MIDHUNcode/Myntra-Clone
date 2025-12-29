5c3a1c1d42121108db7fbd4b65281d5bd11587f2
Midhun <midhun.g3.14@gmail.com>
2025-12-29 09:55:22 +0530

    Add files via upload

--- Myntra/src/types/Product.ts
+++ Myntra/src/types/Product.ts
@@ -1,1 +1,12 @@
+import { ImageSourcePropType } from "react-native";
+
+export interface Product {
+  id: string;
+  title: string;
+  price: number;
+  image: any;
+  brand?: string; 
+  images?: any[]; 
+}
+
 
