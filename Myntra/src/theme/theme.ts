c02caeb3d7631923917a14ce142a4ec7bb9d8004
Midhun <midhun.g3.14@gmail.com>
2025-12-29 09:53:38 +0530

    Add files via upload

--- Myntra/src/theme/theme.ts
+++ Myntra/src/theme/theme.ts
@@ -1,1 +1,33 @@
+export const lightTheme = {
+  mode: "light",
+  background: "#FFFFFF",
+  card: "#F5F5F5",
+  text: "#000000",
+  buttonBackground: "#1e40af",
+  buttonTextColor: "#FFFFFF",
+  borderColor: "#E0E0E0",
+  tint: "#007AFF",
+};
 
+export const darkTheme = {
+  mode: "dark",
+  background: "#1C1C1C",
+  card: "#2A2A2A",
+  text: "#FFFFFF",
+  buttonBackground: "#FFFFFF",
+  buttonTextColor: "#000000",
+  borderColor: "#333333",
+  tint: "#1E90FF",
+};
+
+export type ThemeType = typeof lightTheme;
+
+export const Colors = {
+  light: lightTheme,
+  dark: darkTheme,
+};
+
+export const Fonts = {
+  rounded: "NunitoSans_700Bold",
+  regular: "NunitoSans_400Regular",
+};
