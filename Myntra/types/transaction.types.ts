2116d3c1074d3e8be29a2c74f8a49597a50dc3b1
Midhun <midhun.g3.14@gmail.com>
2025-12-29 09:57:04 +0530

    Add files via upload

--- Myntra/types/transaction.types.ts
+++ Myntra/types/transaction.types.ts
@@ -1,1 +1,8 @@
-
+export type Transaction = {
+  _id: string;
+  amount: number;
+  paymentMode: "ONLINE" | "COD";
+  type: "PAYMENT" | "REFUND";
+  status: "SUCCESS" | "FAILED" | "PENDING";
+  createdAt: string;
+};
