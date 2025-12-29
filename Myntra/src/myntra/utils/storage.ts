aab25630f763bda9c5f2e41d90a8b6a7154a2907
Midhun <midhun.g3.14@gmail.com>
2025-12-29 09:48:40 +0530

    Add files via upload

--- Myntra/src/myntra/utils/storage.ts
+++ Myntra/src/myntra/utils/storage.ts
@@ -1,1 +1,33 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
+export const saveUserData = async (_id: string, name: string, email: string) => {
+  try {
+    await AsyncStorage.setItem('userid', _id);
+    await AsyncStorage.setItem('userName', name);
+    await AsyncStorage.setItem('userEmail', email);
+  } catch (error) {
+    console.log('Error saving user data:', error);
+  }
+};
+
+export const getUserData = async () => {
+  try {
+    const _id = await AsyncStorage.getItem('userid');
+    const name = await AsyncStorage.getItem('userName');
+    const email = await AsyncStorage.getItem('userEmail');
+    return { _id, name, email };
+  } catch (error) {
+    console.log('Error getting user data:', error);
+    return { _id: null, name: null, email: null };
+  }
+};
+
+export const clearUserData = async () => {
+  try {
+    await AsyncStorage.removeItem('userid');
+    await AsyncStorage.removeItem('userName');
+    await AsyncStorage.removeItem('userEmail');
+  } catch (error) {
+    console.log('Error clearing user data:', error);
+  }
+};
